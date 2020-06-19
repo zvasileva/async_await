0528d127b90bda890717c92871c963b08b21f96d
zvasileva <zojka-homka@rambler.ru>
2020-06-19 19:55:02 +0300

    Ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 // TODO: write your code here
-import json from '../parser';
-import read from '../reader';
+import json from './parser';
+import read from './reader';
 
 console.log('worked');
 const dataIn = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
